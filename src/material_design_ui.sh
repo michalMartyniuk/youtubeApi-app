314becc50527304e589f39cbbedda6a028a8acbd
Michał Martyniuk <michal.martyniuk7@gmail.com>
2019-07-27 17:45:03 +0200

    few

---
+++ src/material_design_ui.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#Album example variables
+React="import React from 'react'"
+AppBar="import AppBar from '@material-ui/core/AppBar'"
+Button="import Button from '@material-ui/core/Button'"
+CameraIcon="import CameraIcon from '@material-ui/icons/PhotoCamera'"
+Card="import Card from '@material-ui/core/Card'"
+CardActions="import CardActions from '@material-ui/core/CardActions'"
+CardContenr=import CardContent from '@material-ui/core/CardContent';
+CardMedia="import CardMedia from '@material-ui/core/CardMedia'"
+CssBaseline="import CssBaseline from '@material-ui/core/CssBaseline'"
+Grid="import Grid from '@material-ui/core/Grid'"
+Toolbar="import Toolbar from '@material-ui/core/Toolbar'"
+Typography="import Typography from '@material-ui/core/Typography'"
+MakeStyles="import { makeStyles } from '@material-ui/core/styles'"
+Card="import Container from '@material-ui/core/Container'"
+Link="import Link from '@material-ui/core/Link'"
+
+#Pricing example variables
+# import React from 'react';
+# import AppBar from '@material-ui/core/AppBar';
+# import Button from '@material-ui/core/Button';
+# import Card from '@material-ui/core/Card';
+# import CardActions from '@material-ui/core/CardActions';
+# import CardContent from '@material-ui/core/CardContent';
+# import CardHeader from '@material-ui/core/CardHeader';
+# import CssBaseline from '@material-ui/core/CssBaseline';
+# import Grid from '@material-ui/core/Grid';
+# import StarIcon from '@material-ui/icons/StarBorder';
+# import Toolbar from '@material-ui/core/Toolbar';
+# import Typography from '@material-ui/core/Typography';
+# import Link from '@material-ui/core/Link';
+# import { makeStyles } from '@material-ui/core/styles';
+# import Container from '@material-ui/core/Container';
+# import Box from '@material-ui/core/Box';
+
+#SignIn example variables
+#import React from 'react';
+#import Avatar from '@material-ui/core/Avatar';
+# import Button from '@material-ui/core/Button';
+# import CssBaseline from '@material-ui/core/CssBaseline';
+# import TextField from '@material-ui/core/TextField';
+# import FormControlLabel from '@material-ui/core/FormControlLabel';
+# import Checkbox from '@material-ui/core/Checkbox';
+# import Link from '@material-ui/core/Link';
+# import Grid from '@material-ui/core/Grid';
+# import Box from '@material-ui/core/Box';
+# import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+# import Typography from '@material-ui/core/Typography';
+# import { makeStyles } from '@material-ui/core/styles';
+# import Container from '@material-ui/core/Container';
+
+
+#Install material-ui/core
+#npm install @material-ui/core
+
+#Roboto font
+#link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+
+#Font icons
+#<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
+#Svg icons
+#npm install @material-ui/icons
+
+#Material Example
+# curl https://codeload.github.com/mui-org/material-ui/tar.gz/master | tar -xz --strip=2  material-ui-master/examples/cdn
+# cd cdn
+# open index.html
+
+
+#Create React App example
+# curl https://codeload.github.com/mui-org/material-ui/tar.gz/master | tar -xz --strip=2 material-ui-master/examples/create-react-app
+# cd create-react-app
+#Install it and run:
+# npm install
+# npm start
