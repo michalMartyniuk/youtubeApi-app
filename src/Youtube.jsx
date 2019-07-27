cb075119f34bfb287f64f6118f82dca4d0280914
Michał Martyniuk <michal.martyniuk7@gmail.com>
2019-07-27 17:41:41 +0200

    few

--- src/Youtube.jsx
+++ src/Youtube.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
 const Youtube = props => {
-    // search()
     return (
        <div>siema</div> 
     )
--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -1,27 +1,21 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class Search extends Component {
-    state = {
-        searchValue: "",
-    }
-    handleInput = (e) => {
-        this.setState({searchValue: e.target.value})
-    }    
-    searchYoutube = async(query) => {
-        const { result } = await window.gapi.client.youtube.search.list({
-            "part": "id, snippet",
-            "q": query
-        });
-        const videos = result.items;
-        this.props.videos(videos);
-    }
-    render() {
-        console.log(this.props)
-        return (
-            <div className='search-container'>
-                <input type="text" onChange={this.handleInput} value={this.state.searchValue}/>
-                <button onClick={() => this.searchYoutube(this.state.searchValue)}>Search</button>
-            </div>
-        )
-    }
+export default function Search() {
+    const [inputValue, setInputValue] = useState("Enter value"); 
+
+    return (
+        <div className='search-container'>
+            <input type="text" onChange={e => setInputValue(e.target.value)} value={inputValue}/>
+            <button onClick={() => searchYoutube(inputValue)}>Search</button>
+        </div>
+    )
+}
+
+async function searchYoutube (query) {
+    const { result } = await window.gapi.client.youtube.search.list({
+        "part": "id, snippet",
+        "q": query
+    });
+    const videos = result.items;
+    this.props.videos(videos);
 }
